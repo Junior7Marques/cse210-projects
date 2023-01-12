06fece26814e9f123b86379113a8ef35b08ab14c
Junior7Marques <juniorjr07@live.com>
2023-01-12 00:03:35 -0300

    prep 3.3

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,37 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        int Unumber = -1;
+        List<int> numbers = new List<int>();
+        while (Unumber != 0)
+        {
+            Console.Write("Enter a list of numbers, type 0 when finished:  ");
+            Unumber = int.Parse(Console.ReadLine());
+            if (Unumber !=0)
+            {
+                numbers.Add(Unumber);
+            }
+        }
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+        Console.WriteLine($"Sum = {sum}");
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+        int max = numbers[0];
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+        Console.WriteLine($"The max is: {max}");
     }
 }
